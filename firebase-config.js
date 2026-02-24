2f39b6ac007c8da203a5c40d59911ab9916e2798
Game1-lock <chocolatitoprueba2@gmail.com>
2026-02-24 18:38:42 -0500

    firebase config corregido

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,5 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSy...", // // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
   apiKey: "AIzaSyBCT2eHK_lKlOZxfyhlY_7nYPH7ttOYK-M",
   authDomain: "chocolatito1-1e188.firebaseapp.com",
   projectId: "chocolatito1-1e188",
@@ -18,15 +9,5 @@ const firebaseConfig = {
   measurementId: "G-6H2T4HP8R1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-  authDomain: "juego-flechas.firebaseapp.com",
-  projectId: "juego-flechas",
-  storageBucket: "juego-flechas.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
-};
-
-// Inicializar Firebase
+// Inicializar Firebase (para la versión 9 compat)
 firebase.initializeApp(firebaseConfig);
