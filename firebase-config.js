c64fca14a39cb9fa4d7571094cb73dd9fa4d9ecb
Game1-lock <chocolatitoprueba2@gmail.com>
2026-02-24 16:51:08 -0500

    Update Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,32 @@
-firebase
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSy...", // // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCT2eHK_lKlOZxfyhlY_7nYPH7ttOYK-M",
+  authDomain: "chocolatito1-1e188.firebaseapp.com",
+  projectId: "chocolatito1-1e188",
+  storageBucket: "chocolatito1-1e188.firebasestorage.app",
+  messagingSenderId: "1038704807334",
+  appId: "1:1038704807334:web:651501732929cfad3c9367",
+  measurementId: "G-6H2T4HP8R1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+  authDomain: "juego-flechas.firebaseapp.com",
+  projectId: "juego-flechas",
+  storageBucket: "juego-flechas.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
